211e6bf02062cd75ff5680bc21a2869b09eda5bd
Harrison <harrisonloncas@gmail.com>
2024-02-22 14:43:24 -0500

    Initial stages

---
+++ index.js
@@ -0,0 +1,33 @@
+/*console.log('This is the first log of the app')
+window.alert('This is an alert')*/
+
+
+/*
+document.getElementById('myh1').textContent = 'hello world 1';
+document.getElementById('myp').textContent = 'I like pizza';
+
+
+let age = 25;
+let price = 100.50;
+let gpa = 2.1;
+
+console.log(`This is your age: ${
+*/
+
+
+
+/*
+document.getElementById('logoU').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'white';
+    this.style.opacity = '0.8';
+   // this.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    this.style.transition = 'backgroundColor 1.2s ease-in-out, opacity 1.2s ease-in-out';
+  });
+  
+  document.getElementById('logoU').addEventListener('mouseout', function() {
+    this.style.backgroundColor = 'transparent';
+    this.style.opacity = '1';
+    //this.style.boxShadow = 'none';
+    this.style.transition = 'backgroundColor 1.2s ease-in-out,';
+  });
+  */
